99c8ffabbb6078975c3069ea852e2778b410e34d
yoosof <70627266+Yoosofbidardel@users.noreply.github.com>
2021-09-27 10:35:35 +0330

    Add files via upload

---
+++ main.m
@@ -0,0 +1,4 @@
+%lead(I,II)  % uncomment this to call all leads in one figure
+%pace(II)    % uncomment this for the secend part of hw
+%VCG_T(I,II) %uncomment this for the last part of hw
+%VCG_QRS(I,II)
